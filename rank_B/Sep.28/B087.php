e65b954b5b53ea2fecb31ed27205ebf65ceb0256
yoshimura <yoshimura@smartworx.co.jp>
2023-10-19 09:17:01 +0900

    update

--- rank_B/Sep.28/B087.php
+++ rank_B/Sep.28/B087.php
@@ -1,9 +1,7 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
     getInfo();
     getData();
-    
+    outputMaxNum($height, $width, $digit, $input);
     
     function getInfo() {
         global $height, $width, $digit;
@@ -17,40 +15,37 @@
             $input[] = str_split(trim(fgets(STDIN)));
         }
     }
-    
+
     function getNum($input, $array0, $array1) {
-        global $num;
-        $num .= $input[$array0][$array1];
+        return $input[$array0][$array1];
     }
     
-    function getMaxNum($maxNum, $num) {
-        global $maxNum;
+    function getMaxNum(&$maxNum, $num) {  
         $maxNum = max($maxNum, intval($num));
     }
-   
     
-    $maxNum = -1;
-    for ($line = 0; $line < $height; $line++) {
-        for ($column = 0; $column < $width; $column++) {
-            // 上から下
-            if ($line + $digit <= $height) {
-                $num = "";
-                for ($targetDigit = 0; $targetDigit < $digit; $targetDigit++) {
-                    getNum($input, $line + $targetDigit, $column);
+    function outputMaxNum($height, $width, $digit, $input) {
+        $maxNum = -1;
+        for ($line = 0; $line < $height; $line++) {
+            for ($column = 0; $column < $width; $column++) {
+                // 上から下
+                if ($line + $digit <= $height) {
+                    $num = "";
+                    for ($targetDigit = 0; $targetDigit < $digit; $targetDigit++) {
+                        $num .= getNum($input, $line + $targetDigit, $column);
+                    }
+                    getMaxNum($maxNum, $num);
                 }
-                getMaxNum($maxNum, $num);
-            }
-            // 左から右
-            if ($column + $digit <= $width) {
-                $num = "";
-                for ($targetDigit = 0; $targetDigit < $digit; $targetDigit++) {
-                    getNum($input, $line, $column + $targetDigit);
+                // 左から右
+                if ($column + $digit <= $width) {
+                    $num = "";
+                    for ($targetDigit = 0; $targetDigit < $digit; $targetDigit++) {
+                        $num .= getNum($input, $line, $column + $targetDigit);
+                    }
+                    getMaxNum($maxNum, $num);
                 }
-                getMaxNum($maxNum, $num);
             }
         }
+        echo $maxNum;
     }
-        
-    echo $maxNum;
-    
 ?>
