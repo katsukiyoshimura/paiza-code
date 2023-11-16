c16440e1f9f155293c745f93c36a95f46decd819
yoshimura <yoshimura@smartworx.co.jp>
2023-11-16 15:59:21 +0900

    update

--- rank_B/Oct.26/B024.php
+++ rank_B/Oct.26/B024.php
@@ -1,30 +1,26 @@
 <?php
-    class Area {
-        private $radius;
-        private $innerPart;
+class Area {
+    private $radius;
 
-        public function __construct($radius) {
-            $this->radius = $radius;
-        }
-                
-        public function calculatePaintedArea() {
-            $this->innerPart = 0;
-            for ($x = 1; $x <= ceil($this->radius); $x++) {
-               for ($y = 1; $y <= ceil($this->radius); $y++) {
-                   if (($x - 1) ** 2 + ($y - 1) ** 2 <= $this->radius ** 2) {
-                       $this->innerPart++;
-                   }
-               }
-            }
-        }
-        
-        function calculateTotalPaintedArea() {
-            return $this->innerPart * 4;
-        }
+    public function __construct($radius) {
+        $this->radius = $radius;
     }
-    
-    $radius = fgets(STDIN);
-    $paintedArea = new Area($radius);
-    $paintedArea->calculatePaintedArea();
-    echo $paintedArea->getTotalPaintedArea();
+            
+    public function calculatePaintedArea() {
+        $innerPart = 0;
+        for ($x = 1; $x <= ceil($this->radius); $x++) {
+           for ($y = 1; $y <= ceil($this->radius); $y++) {
+               if (($x - 1) ** 2 + ($y - 1) ** 2 <= $this->radius ** 2) {
+                   $innerPart++;
+               }
+           }
+        }
+        $totalInnerPart = $innerPart * 4;
+        return $totalInnerPart;
+    }
+}
+
+$radius = fgets(STDIN);
+$paintedArea = new Area($radius);
+echo $paintedArea->calculatePaintedArea();
 ?>
